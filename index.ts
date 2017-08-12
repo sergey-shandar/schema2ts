cbe22b4bfd8ddf3cf09825acfa5cc256b842f43b
Sergey Shandar <sergey@live.com.au>
2017-08-12 10:38:50 -0700

    multi-line support

--- index.ts
+++ index.ts
@@ -1,18 +1,49 @@
 import * as fs from "fs";
 import * as os from "os";
 
+function* wrap(i: Iterable<string>, prefix: string, suffix: string) {
+    let previous: string|undefined = undefined;
+    for (const v of i) {
+        if (previous !== undefined) {
+            yield previous;
+            previous = v;
+        } else {
+            previous = prefix + v;
+        }
+    }
+    if (previous !== undefined) {
+        yield previous + suffix;
+    }
+}
+
+function* indent(i: IterableIterator<string>) {
+    for (const v of i) {
+        yield "    " + v;
+    }
+}
+
+function* join(ii: Iterable<Iterable<string>>, separator: string) {
+    let notFirst = false;
+    for (const i of ii) {
+        if (notFirst) {
+            yield separator;
+        } else {
+            notFirst = true;
+        }
+        yield *i;        
+    }
+}
+
 namespace Ts {
     export interface Property {
         readonly name: string;
-        readonly type: string;
+        readonly type: Type;
     }
-    export type Properties = Property[];
-    export interface Interface {        
-        readonly properties: Properties;        
-    }
+    export type Interface = Property[];
     export interface Type {
         readonly ref?: string;
         readonly interface?: Interface;
+        readonly union?: Type[];
     }
     export interface TypeAlias {
         readonly name: string;
@@ -20,26 +51,29 @@ namespace Ts {
     }
     export type Module = TypeAlias[];
 
-    export function* type(t: Type) {        
+    export function* type(t: Type): IterableIterator<string> {        
         if (t.ref !== undefined) {
             yield t.ref;
         } else if(t.interface !== undefined) {
-            yield "{"
-            for (const p of t.interface.properties) {
-                yield "    readonly " + p.name + "?: " + p.type + ";"
+            if (t.interface.length == 0) {
+                yield "{}";
+            } else {
+                yield "{"
+                for (const p of t.interface) {
+                    yield *indent(wrap(type(p.type), "readonly " + p.name + ": ", ";"));
+                }
+                yield "}";
             }
-            yield "}";
+        } else if(t.union !== undefined) {
+            yield "(any)";
         }
     }
 
     export function* typeAlias(t: TypeAlias) {
+        const array = [...type(t.type)];
         const i = t.type.interface;
         if (i !== undefined) {
-            yield "type " + t.name + " = {"
-            for (const p of i.properties) {
-                yield "    readonly " + p.name + "?: " + p.type + ";"
-            }
-            yield "}";
+            yield* wrap(type(t.type), "type " + t.name + " = ", ";");
         }
     }
     export function* module(m: Module) {
@@ -66,17 +100,17 @@ const main = "SchemaObject";
 
 const definitionsUri = "#/definitions/";
 
-function createType(schemaObject: SchemaObject): string {
+function createType(schemaObject: SchemaObject): Ts.Type {
     
     // $ref
     {
         const $ref = schemaObject.$ref;
         if ($ref !== undefined) {
-            if ($ref === "#") return main;
+            if ($ref === "#") return { ref: main };
             if ($ref.startsWith(definitionsUri)) {
-                return $ref.substr(definitionsUri.length);
+                return { ref: $ref.substr(definitionsUri.length) };
             }
-            return "any";
+            return { ref: "any" };
         }
     }
 
@@ -84,33 +118,33 @@ function createType(schemaObject: SchemaObject): string {
     {
         const anyOf = schemaObject.anyOf;
         if (anyOf !== undefined) {
-            return "(" + anyOf.map(createType).join("|") + ")";
+            return { union: anyOf.map(createType) };
         }
     }
 
     const type = schemaObject.type === undefined ? "object" : schemaObject.type;
     switch (type) {
         case "object":
-            let result = "{";
+            let properties: Ts.Property[] = [];
             const additionalProperties = schemaObject.additionalProperties;
             if (additionalProperties !== undefined) {
-                result += "readonly[_:string]:" + createType(additionalProperties) + ";";
+                properties.push({ name: "[_:string]", type: createType(additionalProperties) });
             }
-            return result + "}";
+            return { interface: properties };
         case "array":
             const items = schemaObject.items;
             const type = items !== undefined ? createType(items) : "any";
-            return type + "[]";
+            return { ref: type + "[]" };
     }
 
-    return type;
+    return { ref: type };
 }
 
 const schemaObject : SchemaObject = JSON.parse(fs.readFileSync("schema.json").toString());
 
-function createProperties(properties: {readonly[_:string]: SchemaObject}|undefined) {
+function createProperties(properties: {readonly[_:string]: SchemaObject}|undefined) : Ts.Property[] {
     return properties !== undefined 
-        ? Object.keys(properties).map(name => ({ name: name, type: createType(properties[name]) }))
+        ? Object.keys(properties).map(name => ({ name: name + "?", type: createType(properties[name]) }))
         : [];
 }
 
@@ -118,9 +152,7 @@ function createTypeAlias(name: string, schema: SchemaObject): Ts.TypeAlias {
     return { 
         name: name,
         type: {
-            interface: { 
-                properties: createProperties(schema.properties)
-            }
+            interface: createProperties(schema.properties)
         }
     };
 }
--- schema.d.ts
+++ schema.d.ts
@@ -1,14 +1,9 @@
 
-type schemaArray = {
-}
-type nonNegativeInteger = {
-}
-type nonNegativeIntegerDefault0 = {
-}
-type simpleTypes = {
-}
-type stringArray = {
-}
+type schemaArray = {};
+type nonNegativeInteger = {};
+type nonNegativeIntegerDefault0 = {};
+type simpleTypes = {};
+type stringArray = {};
 type SchemaObject = {
     readonly $id?: string;
     readonly $schema?: string;
@@ -25,7 +20,7 @@ type SchemaObject = {
     readonly minLength?: nonNegativeIntegerDefault0;
     readonly pattern?: string;
     readonly additionalItems?: SchemaObject;
-    readonly items?: (SchemaObject|schemaArray);
+    readonly items?: (any);
     readonly maxItems?: nonNegativeInteger;
     readonly minItems?: nonNegativeIntegerDefault0;
     readonly uniqueItems?: boolean;
@@ -34,17 +29,25 @@ type SchemaObject = {
     readonly minProperties?: nonNegativeIntegerDefault0;
     readonly required?: stringArray;
     readonly additionalProperties?: SchemaObject;
-    readonly definitions?: {readonly[_:string]:SchemaObject;};
-    readonly properties?: {readonly[_:string]:SchemaObject;};
-    readonly patternProperties?: {readonly[_:string]:SchemaObject;};
-    readonly dependencies?: {readonly[_:string]:(SchemaObject|stringArray);};
+    readonly definitions?: {
+        readonly [_:string]: SchemaObject;
+    };
+    readonly properties?: {
+        readonly [_:string]: SchemaObject;
+    };
+    readonly patternProperties?: {
+        readonly [_:string]: SchemaObject;
+    };
+    readonly dependencies?: {
+        readonly [_:string]: (any);
+    };
     readonly propertyNames?: SchemaObject;
     readonly const?: {};
     readonly enum?: any[];
-    readonly type?: (simpleTypes|simpleTypes[]);
+    readonly type?: (any);
     readonly format?: string;
     readonly allOf?: schemaArray;
     readonly anyOf?: schemaArray;
     readonly oneOf?: schemaArray;
     readonly not?: SchemaObject;
-}
+};
