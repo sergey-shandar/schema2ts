41aefa224cc9e702250b915867675d20f58589ec
Sergey Shandar <sergey@live.com.au>
2017-08-12 06:35:09 -0700

    additionalProperties

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import * as fs from "fs";
 
 namespace Ts {
+    export interface Property {
+        readonly name: string;
+        readonly type: string;
+    }
     export interface Properties {
         readonly [name: string]: string;
     }
@@ -11,6 +15,7 @@ namespace Ts {
         readonly name: string;
         readonly properties: Properties;
     }
+    export type Module = Interface[];
     export function* interface_(i: Interface) {
         yield "interface " + i.name + "{"
         const p = i.properties;
@@ -31,10 +36,21 @@ interface SchemaObject {
     readonly $id?: string;
     readonly type?: string;
     readonly properties?: SchemaObjectMap;
+    readonly additionalProperties?: SchemaObject;
 }
 
 function createType(schemaObject: SchemaObject) {
-    return schemaObject.type === undefined ? "object" : schemaObject.type;
+    const type = schemaObject.type === undefined ? "object" : schemaObject.type;
+    switch (type) {
+        case "object":
+            let result = "{";
+            const additionalProperties = schemaObject.additionalProperties;
+            if (additionalProperties) {
+                result += "readonly[_:string]:" + createType(additionalProperties) + ";";
+            }
+            return result + "}";
+    }
+    return type;
 }
 
 const object : SchemaObject = JSON.parse(fs.readFileSync("schema.json").toString());
@@ -46,7 +62,7 @@ if (schemaProperties !== undefined) {
         tsProperties[name] = createType(schemaProperties[name]);
     }
 }
-const result = Ts.interface_({name: "Schema", properties: tsProperties});
+const result = Ts.interface_({name: "SchemaObject", properties: tsProperties});
 
 for (const line of result) {
     console.log(line);
